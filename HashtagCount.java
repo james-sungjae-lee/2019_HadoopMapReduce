7bae780f5cbcea0395e863e95214ff597664db17
Sungjae Lee <odobenuseKR@gmail.com>
2019-06-03 13:53:23 +0900

    Lab1 'HashtagCount'

---
+++ HashtagCount.java
@@ -0,0 +1,75 @@
+package kr.ac.kookmin.cs.bigdata;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class HashtagCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        // Create a new job
+        Job job = Job.getInstance(conf, "hashtagcount");
+
+        // Use the HashtagCount.class file to point to the job jar
+        job.setJarByClass(HashtagCount.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        // Setting the input and output locations
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // Submit the job and wait for it's completion
+        job.waitForCompletion(true);
+    }
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text hashtag = new Text();
+
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] words = line.split("//S+");
+
+            for (String word : words) {
+                if(word.length() > 1 && word.startsWith('#')){
+                    hashtag.set(word);
+                    context.write(hashtag, one);
+                }
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+
+            // Sum all the occurrences of the hashtag (key)
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+}
